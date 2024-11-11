a79ee5704c0d9d04163db207f6ad814342593785
luqmanitogobimbel <luqmangrahito.gobimbel@gmail.com>
2024-11-11 16:51:14 +0700

    change dto

--- src/dto/master-gedung-dto.ts
+++ src/dto/master-gedung-dto.ts
@@ -1,3 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PaginationDto } from './pagination-dto';
+import { IsInt, IsOptional, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class BagianGedungDto extends PaginationDto {}
+export class DetailGedungDto extends PaginationDto {
+  @ApiProperty({ required: false, example: 1 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  bagian_gedung_komponen_id: number;
+}
+export class KomponenGedungDto extends PaginationDto {
+  @ApiProperty({ required: false, example: 1 })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  bagian_gedung_id: number;
+}
