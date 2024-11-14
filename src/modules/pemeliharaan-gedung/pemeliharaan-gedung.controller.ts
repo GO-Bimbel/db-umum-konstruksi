ea2a1615e56ba0621d664a9b339ed3a381a9675a
luqmanitogobimbel <luqmangrahito.gobimbel@gmail.com>
2024-11-14 11:50:13 +0700

    move loc

--- src/modules/pemeliharaan-gedung/pemeliharaan-gedung.controller.ts
+++ src/modules/pemeliharaan-gedung/pemeliharaan-gedung.controller.ts
@@ -17,12 +17,13 @@ import {
   PemeliharaanGedungDto,
   UpdatePemeliharaanGedungDto,
 } from 'src/dto/pemeliharaan-gedung.dto';
+import { DetailListGedungDto, ImageDetailDto } from 'src/dto/master-gedung-dto';
 
 @Controller('pemeliharaan-gedung')
 @ApiTags('Master Gedung')
 export class PemeliharaanGedungController {
   constructor(
-    private readonly masterGedungService: PemeliharaanGedungService,
+    private readonly pemeliharaanGedungService: PemeliharaanGedungService,
   ) {}
 
   @Get()
@@ -33,7 +34,7 @@ export class PemeliharaanGedungController {
   async getPemeliharaanGedung(@Query() params: PemeliharaanGedungDto) {
     try {
       const { total_data, data } =
-        await this.masterGedungService.getPemeliharaanGedung(params);
+        await this.pemeliharaanGedungService.getPemeliharaanGedung(params);
 
       const metadata = {
         total_count: total_data,
@@ -61,6 +62,77 @@ export class PemeliharaanGedungController {
     }
   }
 
+  @Get('komponen')
+  @ApiOperation({
+    summary: 'Get Komponen Gedung',
+    description: 'Get komponen gedung using query params',
+  })
+  async getKomponen(@Query() params: DetailListGedungDto) {
+    try {
+      const { total_data, data } =
+        await this.pemeliharaanGedungService.getKomponen(params);
+
+      const metadata = {
+        total_count: total_data,
+        page_count: params.is_all_data
+          ? 1
+          : Math.ceil(total_data / (params.per_page ?? 10)),
+        page: params.is_all_data ? 1 : params.page,
+        per_page: params.is_all_data ? total_data : params.per_page,
+        sort: params.sort,
+        order_by: params.order_by,
+        keyword: params.keyword,
+      };
+
+      return {
+        data: data,
+        metadata: metadata ? metadata : null,
+        _meta: {
+          code: HttpStatus.OK,
+          status: SUCCESS_STATUS,
+          message: 'success get komponen gedung',
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Get('komponen/detail')
+  @ApiOperation({
+    summary: 'Get Komponen Detail',
+    description: 'Get komponen detail using query params',
+  })
+  async getKomponenDetail(@Query() params: ImageDetailDto) {
+    try {
+      const { total_data, data } =
+        await this.pemeliharaanGedungService.getKomponenDetail(params);
+
+      const metadata = {
+        total_count: total_data,
+        page_count: params.is_all_data
+          ? 1
+          : Math.ceil(total_data / (params.per_page ?? 10)),
+        page: params.is_all_data ? 1 : params.page,
+        per_page: params.is_all_data ? total_data : params.per_page,
+        sort: params.sort,
+        order_by: params.order_by,
+        keyword: params.keyword,
+      };
+
+      return {
+        data: data,
+        metadata: metadata ? metadata : null,
+        _meta: {
+          code: HttpStatus.OK,
+          status: SUCCESS_STATUS,
+          message: 'success get komponen detail',
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
   @Post()
   @ApiOperation({
     summary: 'Input pemeliharaan gedung',
@@ -68,7 +140,8 @@ export class PemeliharaanGedungController {
   })
   async createPemeliharaanGedung(@Body() dto: CreatePemeliharaanGedungDto) {
     try {
-      const data = await this.masterGedungService.createPemeliharaanGedung(dto);
+      const data =
+        await this.pemeliharaanGedungService.createPemeliharaanGedung(dto);
       return {
         data: data,
         _meta: {
@@ -93,10 +166,11 @@ export class PemeliharaanGedungController {
     @Body() dto: UpdatePemeliharaanGedungDto,
   ) {
     try {
-      const data = await this.masterGedungService.updatePemeliharaanGedung(
-        Number(id),
-        dto,
-      );
+      const data =
+        await this.pemeliharaanGedungService.updatePemeliharaanGedung(
+          Number(id),
+          dto,
+        );
       return {
         data: data,
         _meta: {
