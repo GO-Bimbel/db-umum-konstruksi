a5aa4cbd0c39b68c76606624b6bce5adf4888f1e
luqmanitogobimbel <luqmangrahito.gobimbel@gmail.com>
2024-11-14 11:11:57 +0700

    add komponen detail

--- src/modules/master-gedung/master-gedung,controller.ts
+++ src/modules/master-gedung/master-gedung,controller.ts
@@ -5,6 +5,8 @@ import {
   BagianGedungDto,
   DetailGedungDto,
   KomponenGedungDto,
+  DetailListGedungDto,
+  ImageDetailDto,
 } from 'src/dto/master-gedung-dto';
 import { MasterGedungService } from './master-gedung.service';
 import { SUCCESS_STATUS } from 'src/dto/request-response.dto';
@@ -115,6 +117,77 @@ export class MasterGedungController {
           code: HttpStatus.OK,
           status: SUCCESS_STATUS,
           message: 'success get Bagian Gedung Detail',
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  @Get('list-detail')
+  @ApiOperation({
+    summary: 'Get Bagian List Gedung Detail',
+    description: 'Get Bagian list gedung detail using query params',
+  })
+  async getListDetail(@Query() params: DetailListGedungDto) {
+    try {
+      const { total_data, data } =
+        await this.masterGedungService.getListDetail(params);
+
+      const metadata = {
+        total_count: total_data,
+        page_count: params.is_all_data
+          ? 1
+          : Math.ceil(total_data / (params.per_page ?? 10)),
+        page: params.is_all_data ? 1 : params.page,
+        per_page: params.is_all_data ? total_data : params.per_page,
+        sort: params.sort,
+        order_by: params.order_by,
+        keyword: params.keyword,
+      };
+
+      return {
+        data: data,
+        metadata: metadata ? metadata : null,
+        _meta: {
+          code: HttpStatus.OK,
+          status: SUCCESS_STATUS,
+          message: 'success get Bagian List Gedung Detail',
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+  @Get('image-detail')
+  @ApiOperation({
+    summary: 'Get Image Detail',
+    description: 'Get image detail using query params',
+  })
+  async getImageDetail(@Query() params: ImageDetailDto) {
+    try {
+      const { total_data, data } =
+        await this.masterGedungService.getImageDetail(params);
+
+      const metadata = {
+        total_count: total_data,
+        page_count: params.is_all_data
+          ? 1
+          : Math.ceil(total_data / (params.per_page ?? 10)),
+        page: params.is_all_data ? 1 : params.page,
+        per_page: params.is_all_data ? total_data : params.per_page,
+        sort: params.sort,
+        order_by: params.order_by,
+        keyword: params.keyword,
+      };
+
+      return {
+        data: data,
+        metadata: metadata ? metadata : null,
+        _meta: {
+          code: HttpStatus.OK,
+          status: SUCCESS_STATUS,
+          message: 'success get image Detail',
         },
       };
     } catch (error) {
--- src/modules/master-gedung/master-gedung.service.ts
+++ src/modules/master-gedung/master-gedung.service.ts
@@ -4,6 +4,8 @@ import {
   BagianGedungDto,
   DetailGedungDto,
   KomponenGedungDto,
+  DetailListGedungDto,
+  ImageDetailDto,
 } from 'src/dto/master-gedung-dto';
 
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -64,6 +66,98 @@ export class MasterGedungService {
     return {
       total_data: total_data,
       data: data,
+    };
+  }
+  async getListDetail(params: DetailListGedungDto) {
+    const skip = params.page ? (params.page - 1) * params.per_page : 0;
+
+    const where: Prisma.bagian_gedung_detailWhereInput = {
+      pemeliharaan_gedung: {
+        some: {
+          gedung_id: params.gedung_id,
+        },
+      },
+      bagian_gedung_komponen: {
+        bagian_gedung: {
+          id: params.bagian_gedung_id,
+        },
+      },
+    };
+
+    const [data, total_data] = await Promise.all([
+      this.prisma.bagian_gedung_detail.findMany({
+        where,
+        select: {
+          id: true,
+          nama: true,
+          bagian_gedung_komponen: {
+            select: {
+              nama: true,
+            },
+          },
+          pemeliharaan_gedung: {
+            select: {
+              id: true,
+              bagian_gedung_detail_id: true,
+              gedung_id: true,
+              image_url: true,
+            },
+          },
+        },
+
+        skip: params.is_all_data ? undefined : skip,
+        take: params.is_all_data ? undefined : params.per_page,
+      }),
+      this.prisma.bagian_gedung_detail.count({
+        where,
+      }),
+    ]);
+
+    const output = data.map((item) => {
+      return {
+        id: item.id,
+        nama_komponen: item.bagian_gedung_komponen.nama,
+        detail_komponen: item.nama,
+        detail_pemeliharaan_id: item.pemeliharaan_gedung.filter(
+          (list) =>
+            list.bagian_gedung_detail_id === item.id &&
+            list.gedung_id === params.gedung_id,
+        )[0]?.id,
+      };
+    });
+
+    return {
+      total_data: total_data,
+      data: output,
+    };
+  }
+  async getImageDetail(params: ImageDetailDto) {
+    const skip = params.page ? (params.page - 1) * params.per_page : 0;
+
+    const where: Prisma.pemeliharaan_gedungWhereInput = {
+      id: params.id,
+    };
+
+    const [data, total_data] = await Promise.all([
+      this.prisma.pemeliharaan_gedung.findMany({
+        where,
+        select: {
+          kondisi: true,
+          catatan: true,
+          image_url: true,
+          updated_at: true,
+        },
+        skip: params.is_all_data ? undefined : skip,
+        take: params.is_all_data ? undefined : params.per_page,
+      }),
+      this.prisma.pemeliharaan_gedung.count({
+        where,
+      }),
+    ]);
+
+    return {
+      total_data: total_data,
+      data,
     };
   }
 
