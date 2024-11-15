f10782d6a6d3efd522f26c66fb58778f2bce533c
saadgobimbel <saad.gobimbel@gmail.com>
2024-11-15 18:34:24 +0700

    FEAT: Add health

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,23 @@ import { LoggerService } from './common/logger/logger.service';
 import { ApiModule } from './api/api.module';
 import { MasterGedungModule } from './modules/master-gedung/master-gedung.module';
 import { PemeliharaanGedungModule } from './modules/pemeliharaan-gedung/pemeliharaan-gedung.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TerminusModule.forRoot({
+      gracefulShutdownTimeoutMs: 1000,
+    }),
     PrismaModule,
     MasterGedungModule,
     PemeliharaanGedungModule,
     ApiModule,
   ],
+  controllers: [HealthController],
   providers: [LoggerService],
 })
 export class AppModule {}
